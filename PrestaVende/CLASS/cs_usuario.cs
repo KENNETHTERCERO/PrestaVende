96e50d02716dc51e069a05fa242d6f04844ab59c
kennethtercero <kennethtercero@gmail.com>
2019-09-08 16:15:01 -0600

    KT-Se agrego recepcion de caja a usuario operador.

--- PrestaVende/CLASS/cs_usuario.cs
+++ PrestaVende/CLASS/cs_usuario.cs
@@ -9,16 +9,32 @@ namespace PrestaVende.CLASS
 {
     public class cs_usuario
     {
-        public static int       id_usuario = 0;
-        public static int       id_empresa = 0;
-        public static int       id_sucursal = 0;
-        public static int       id_rol = 0;
-        public static string    usuario = "";
-        public static string    primer_nombre = "";
+        public static int       id_usuario      = 0;
+        public static int       id_empresa      = 0;
+        public static int       id_sucursal     = 0;
+        public static int       id_rol          = 0;
+        public static int       id_caja         = 0;
+        public static int       puede_vender    = 0;
+        private static decimal  saldo_caja      = 0;
+        public static string    usuario         = "";
+        public static string    primer_nombre   = "";
         public static string    primer_apellido = "";
-
+        
         private cs_connection connect;
         private SqlCommand command = new SqlCommand();
+
+        public static decimal Saldo_caja
+        {
+            get
+            {
+                return saldo_caja;
+            }
+
+            set
+            {
+                saldo_caja = value;
+            }
+        }
 
         public cs_usuario()
         {
@@ -29,7 +45,7 @@ namespace PrestaVende.CLASS
         {
             string errorExec = "";
             string[] error;
-            error = new string[9];
+            error = new string[13];
 
             try
             {
@@ -39,19 +55,24 @@ namespace PrestaVende.CLASS
                 connect.connection.Open();
 
                 command.Connection = connect.connection;
-                command.CommandText = "SELECT " +
-                                            "id_usuario, " +        //0, 1
-	                                        "id_empresa, " +        //1, 2
-	                                        "id_sucursal, " +       //2, 3
-	                                        "usuario, " +           //3, 4
-	                                        "primer_nombre," +      //4, 5
-	                                        "primer_apellido, " +   //5, 6
-	                                        "id_rol, " +            //6, 7
-	                                        "estado " +             //7, 8
+                command.CommandText = "SELECT TOP 1 " +
+                                            "usu.id_usuario, " +        //0, 1
+	                                        "usu.id_empresa, " +        //1, 2
+	                                        "usu.id_sucursal, " +       //2, 3
+	                                        "usu.usuario, " +           //3, 4
+	                                        "usu.primer_nombre," +      //4, 5
+	                                        "usu.primer_apellido, " +   //5, 6
+	                                        "usu.id_rol, " +            //6, 7
+                                            "usu.estado, " +            //7, 8
+                                            "ISNULL(asi.id_caja, 0), " +           //8, 9
+                                            "ISNULL(asi.id_asignacion_caja, 0), " +  //9, 10
+                                            "ISNULL(asi.estado_asignacion, 0), " + //10, 11
+                                            "usu.caja_asignada " + //11, 12
                                         "FROM " +
-                                        "tbl_usuario " + 
-                                        "WHERE usuario = @usuario and password_user = @password "+
-                                              "AND estado = 1";
+                                        "tbl_usuario AS usu " +
+                                        "LEFT JOIN tbl_asignacion_caja AS asi ON asi.id_usuario_asignado = usu.id_usuario AND asi.id_estado_caja in (2,3) AND asi.estado_asignacion IN (0,1) " +
+                                        "WHERE usu.usuario = @usuario and usu.password_user = @password " +
+                                              "AND usu.estado = 1 ORDER BY asi.fecha_creacion DESC";
                 command.Parameters.AddWithValue("@usuario", user);
                 command.Parameters.AddWithValue("@password", password);
                 dtUser.Load(command.ExecuteReader());
@@ -69,6 +90,10 @@ namespace PrestaVende.CLASS
                         error[6] = item[5].ToString();
                         error[7] = item[6].ToString();
                         error[8] = item[7].ToString();
+                        error[9] = item[8].ToString();
+                        error[10] = item[9].ToString();
+                        error[11] = item[10].ToString();
+                        error[12] = item[11].ToString();
                     }
                 }
                 else
--- PrestaVende/WebLogin.aspx.cs
+++ PrestaVende/WebLogin.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,7 +10,11 @@ namespace PrestaVende
 {
     public partial class WebLogin : System.Web.UI.Page
     {
-        CLASS.cs_usuario login = new CLASS.cs_usuario();
+        private CLASS.cs_usuario login = new CLASS.cs_usuario();
+        private CLASS.cs_asignacion_caja asignacion_caja = new CLASS.cs_asignacion_caja();
+        private static DateTime fecha_asignacion, fecha_hoy;
+
+        private static string id_asignacion = "0", estado_asignacion = "", caja_asignada = "", error = "";
 
         protected void Page_Load(object sender, EventArgs e)
         {
@@ -34,7 +39,8 @@ namespace PrestaVende
             {
                 if (validateTextBox())
                 {
-                    string[] respuesta = new string[10];
+                    string[] respuesta = new string[11];
+                    
                     respuesta = login.Login(txtUser.Text, txtPassword.Text);
 
 
@@ -47,23 +53,67 @@ namespace PrestaVende
                         //string id = Convert.ToString(getId.getIdUser(txtUser.Text, txtPassword.Text));
                         //string id_ubicacion = Convert.ToString(getId.getIdUbicacion(txtUser.Text, txtPassword.Text));
                         HttpCookie userLogin = new HttpCookie("userLogin");
-                        CLASS.cs_usuario.id_usuario     = Convert.ToInt32(respuesta[1]);
-                        CLASS.cs_usuario.id_empresa     = Convert.ToInt32(respuesta[2]);
-                        CLASS.cs_usuario.id_sucursal    = Convert.ToInt32(respuesta[3]);
-                        CLASS.cs_usuario.usuario        = respuesta[4];
-                        CLASS.cs_usuario.primer_nombre  = respuesta[5];
-                        CLASS.cs_usuario.primer_apellido = respuesta[6];
-                        CLASS.cs_usuario.id_rol         = Convert.ToInt32( respuesta[7]);
+                        CLASS.cs_usuario.id_usuario         = Convert.ToInt32(respuesta[1]);
+                        CLASS.cs_usuario.id_empresa         = Convert.ToInt32(respuesta[2]);
+                        CLASS.cs_usuario.id_sucursal        = Convert.ToInt32(respuesta[3]);
+                        CLASS.cs_usuario.usuario            = respuesta[4];
+                        CLASS.cs_usuario.primer_nombre      = respuesta[5];
+                        CLASS.cs_usuario.primer_apellido    = respuesta[6];
+                        CLASS.cs_usuario.id_rol             = Convert.ToInt32( respuesta[7]);
+                        CLASS.cs_usuario.id_caja            = Convert.ToInt32(respuesta[9]);
+                        id_asignacion                       = respuesta[10];
+                        estado_asignacion                   = respuesta[11];
+                        caja_asignada                       = respuesta[12];
 
                         Session["id_rol"] = CLASS.cs_usuario.id_rol;
-                        
-                        
                         userLogin.Expires = DateTime.Now.AddHours(3);
                         Response.Cookies.Add(userLogin);
-                        Response.Redirect("~/Public/WFPrincipal.aspx", false);
+
+                        getDatosCaja();
+                        ingresarPrincipal();
                     }
 
 
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private void ingresarPrincipal()
+        {
+            try
+            {
+                if (CLASS.cs_usuario.id_caja == 0 && CLASS.cs_usuario.id_rol != 5)
+                {
+                    Response.Redirect("~/Public/WFPrincipal.aspx", false);
+                }
+                else
+                {
+                    if (!id_asignacion.Equals("0") && estado_asignacion.Equals("0"))
+                    {
+                        Response.Redirect("~/Public/RecepcionCaja.aspx?id_asignacion=" + id_asignacion, false);
+                    }
+                    else if (caja_asignada.Equals("1"))
+                    {
+                        fecha_hoy = DateTime.Now;
+
+                        if (fecha_asignacion.Date < fecha_hoy.Date)
+                        {
+                            showWarning("Usted aun tiene la caja asignada, por favor solicite al gerente que la cierre.");
+                        }
+                        else
+                        {
+                            Response.Redirect("~/Public/WFPrincipal.aspx", false);
+                        }
+                    }
+                    else
+                    {
+                        showWarning("Usted no tiene caja asignada.");
+                    }
                 }
             }
             catch (Exception)
@@ -93,6 +143,22 @@ namespace PrestaVende
             else
             {
                 return true;
+            }
+        }
+
+        private void getDatosCaja()
+        {
+            try
+            {
+                foreach (DataRow item in asignacion_caja.getAsignacionCaja(ref error, id_asignacion).Rows)
+                {
+                    fecha_asignacion = Convert.ToDateTime(item[5].ToString());
+                    CLASS.cs_usuario.Saldo_caja = Convert.ToDecimal(item[3].ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                showError(ex.ToString());
             }
         }
 
