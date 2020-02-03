fcf1598252a730cde820c2d6e5aaeacf8b2784e0
felipe17394 <felipe17394@gmail.com>
2020-02-02 23:16:30 -0600

    Correcciones de pruebas

--- PrestaVende/Public/ReporteRIE.aspx.cs
+++ PrestaVende/Public/ReporteRIE.aspx.cs
@@ -25,7 +25,7 @@ namespace PrestaVende.Public
 
                 if (cookie == null && Convert.ToInt32(Session["id_usuario"]) == 0)
                 {
-                    //Response.Redirect("~/WFWebLogin.aspx");
+                    Response.Redirect("~/WFWebLogin.aspx");
                 }
 
                 if (IsPostBack)
@@ -66,16 +66,15 @@ namespace PrestaVende.Public
         {
             try
             {
-                int id_empresa = Convert.ToInt32(Session["id_empresa"]);
-                id_empresa = 1;
+                int id_empresa = Convert.ToInt32(Session["id_empresa"]);                
 
                 ddlSucursal.DataSource = cs_sucursal.ObtenerSucursalesPorEmpresa(ref error, id_empresa.ToString());
                 ddlSucursal.DataValueField = "id_sucursal";
                 ddlSucursal.DataTextField = "sucursal";
                 ddlSucursal.DataBind();
 
-                //ddlSucursal.SelectedValue = Session["id_sucursal"].ToString();
-                ddlSucursal.SelectedValue = "1";
+                ddlSucursal.SelectedValue = Session["id_sucursal"].ToString();
+                
 
 
                 if (Convert.ToInt32(Session["id_rol"]) == 3 || Convert.ToInt32(Session["id_rol"]) == 4 || Convert.ToInt32(Session["id_rol"]) == 5)
