3c9ada6afad5b958cdd96149b5f9d0da771a17f1
KT <kennethtercero@gmail.com>
2020-03-09 23:11:57 -0600

    KT-Se agrego mantenimiento de casillas.

--- PrestaVende/CLASS/cs_casilla.cs
+++ PrestaVende/CLASS/cs_casilla.cs
@@ -34,5 +34,29 @@ namespace PrestaVende.CLASS
                 return null;
             }
         }
+
+        public int insertCasillas(ref string error, string id_categoria, string letra, int numero_inicial, int numero_final)
+        {
+            try
+            {
+                DataTable contador = new DataTable("casillasCreadas");
+                connection.connection.Open();
+                command.Connection = connection.connection;
+                command.Parameters.Clear();
+                command.CommandText = "exec SP_CrearCasillas @id_sucursal, @id_categoria, @letra, @numero_inicial, @numero_final";
+                command.Parameters.AddWithValue("@id_sucursal", Convert.ToInt32(HttpContext.Current.Session["id_sucursal"]));
+                command.Parameters.AddWithValue("@id_categoria", id_categoria);
+                command.Parameters.AddWithValue("@letra", letra);
+                command.Parameters.AddWithValue("@numero_inicial", numero_inicial);
+                command.Parameters.AddWithValue("@numero_final", numero_final);
+                contador.Load(command.ExecuteReader());
+                return contador.Rows.Count;
+            }
+            catch (Exception ex)
+            {
+                error = ex.ToString();
+                return 0;
+            }
+        }
     }
 }
---
+++ PrestaVende/Public/IncrementosDecrementos.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Public/PrestaVende.Master" AutoEventWireup="true" CodeBehind="IncrementosDecrementos.aspx.cs" Inherits="PrestaVende.Public.IncrementosDecrementos" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+</asp:Content>
