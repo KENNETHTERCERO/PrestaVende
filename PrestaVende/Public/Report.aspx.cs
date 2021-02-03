eeb0bb40c5e0b92a5ace4a55947749187dca547b
KT <kennethtercero@gmail.com>
2021-02-02 18:35:01 -0600

    KT-SE AGREGO OPCION PARA REPORTERIA EN REPORTING SERVICES.

---
+++ PrestaVende/Public/Report.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Net;
+using Microsoft.Reporting.WebForms;
+
+namespace PrestaVende.Public
+{
+    public partial class Report : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!this.IsPostBack)
+            {
+                ReportViewer1.Width = 800;
+                ReportViewer1.Height = 600;
+                ReportViewer1.ProcessingMode = ProcessingMode.Remote;
+                IReportServerCredentials irsc = new CustomReportCredentials("gtsa2019-002", "Tercero#3", "SSRS Server"); // e.g.: ("demo-001", "123456789", "sql5090")
+                ReportViewer1.ServerReport.ReportServerCredentials = irsc;
+                ReportViewer1.ServerReport.ReportServerUrl = new Uri("http://sql5090.site4now.net/ReportServer");
+                ReportViewer1.ServerReport.ReportPath = "/gtsa2019-002/RptReporteFacturasPorFecha"; //e.g.: /demo-001/test
+                ReportViewer1.ServerReport.Refresh();
+            }
+        }
+    }
+
+    public class CustomReportCredentials : IReportServerCredentials
+    {
+        private string _UserName;
+        private string _PassWord;
+        private string _DomainName;
+
+        public CustomReportCredentials(string UserName, string PassWord, string DomainName)
+        {
+            _UserName = UserName;
+            _PassWord = PassWord;
+            _DomainName = DomainName;
+        }
+
+        public System.Security.Principal.WindowsIdentity ImpersonationUser
+        {
+            get { return null; }
+        }
+
+        public ICredentials NetworkCredentials
+        {
+            get { return new NetworkCredential(_UserName, _PassWord, _DomainName); }
+        }
+
+        public bool GetFormsCredentials(out Cookie authCookie, out string user,
+         out string password, out string authority)
+        {
+            authCookie = null;
+            user = password = authority = null;
+            return false;
+        }
+    }
+}
